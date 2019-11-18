afc5a9fdafd2c0ae5ed4be73c87a45b852aafb01
Vlad Belous <34047920+vladbelous@users.noreply.github.com>
2019-11-17 22:08:25 -0800

    Add dockerfile for alpine-based toolchain

---
+++ docker-avr-gcc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKER_IMAGE=alpine-avr-gcc 
+MCU=attiny1614
+FILES=$@
+
+echo $DOCKER_IMAGE
+echo $MCU
+echo $FILES
+
+docker run --rm -v $(pwd):/var/avr \
+  avr-g++ -c -mmcu=$MCU -Os -flto -ffunction-sections $FILES
+
+#avr-g++ -mmcu=attiny1614 -flto -o main.elf main.o
+#avr-objdump -S main.elf > main.S
